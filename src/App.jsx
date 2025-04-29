4f86af5c051c1c32e6b4671153e1a92221716c67
Amisha17355 <amishagupta1711@gmail.com>
2025-04-29 11:44:36 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom'
 import Home from './Pages/Home/Home';
 import About from './Pages/about/About';
 import Services from './Pages/Services/Services';
-import Contact from './Pages/ContactUs/Contact';
+import Contact from './Pages/ContactUS/Contact';
 import Blogs from './Pages/Blogs/Blogs';
 import Ecommerce from './Pages/Ecommerce-development/Ecommerce';
 import Seo from './Pages/SEO/Seo';
 import Web from './Pages/Web-development/Web';
 import Digital from './Pages/Digital-Marketing/Digital';
-import Mobile from './Pages/Mobile-App-Development/Mobile';
-import BI from './Pages/Business-Intelligence/BI';
-import Health from './Pages/Healthcare-Sector/Health';
-import Financial from './Pages/Financial-Sector/Financial';
 import SMO from './Pages/SMO/SMO';
 import Log from "./Pages/Log-In/Log";
 import Test from "./Pages/Testing/Test"
@@ -42,10 +38,6 @@ function App() {
         <Route path="/web-development" element={<Web />} />
         <Route path="/digital-marketing" element={<Digital />} />
         <Route path="/social-media-optimization" element={<SMO />} />
-        <Route path="/mobile-app-development" element={<Mobile />} />
-        <Route path="/business-intelligence" element={<BI />} />
-        <Route path="/financial-sector" element={<Financial />} />
-        <Route path="/healthcare-sector" element={<Health />} />
         <Route path="/log-in" element={<Log />} />
         <Route path="/testing" element={<Test />} />
         <Route path="/privacy-policy" element={<Privacy />} />
